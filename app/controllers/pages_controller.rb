20d8f724724c7cdbb975c3520bda8b5b5282d6d8
Richard Vogt <richvogtjr@gmail.com>
2013-07-13 19:09:31 -0500

    init commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def index
+    @grandma = params[:grandma]
+  end
+
+  def response
+    #input = params[:user_input]
+    #if input == input.upcase
+      @grandma = "NOT SINCE 1966!"
+    #else
+     # @grandma = "SPEAK UP, SONNY!"
+    #end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningRails::Application.config.secret_token = 'beb59fc506e372385325f1a87798f297e44fa582bf03a4785fae764ac3ffd6f46edd1e5efcf117f632480e97d3d99157a70a8270f992e4197f8689e90d3ea0d6'
